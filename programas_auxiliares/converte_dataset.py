47036e88b6d0f5e36bf5595858eee2896d2528ab
EdersonCR <edersonrodrigues10@hotmail.com>
2020-08-28 21:45:56 -0300

    Refatorado script converte_dataset

--- programas_auxiliares/converte_dataset.py
+++ programas_auxiliares/converte_dataset.py
@@ -1,9 +1,45 @@
 import sys
 import os
 
-''' Função que converte os datasets para um formato simplificado
-    Entrada: caminhoEntrada = caminho relativo do diretório dos datasets que serão convertidos
-             caminhoSaida = caminho relativo do diretório onde serão salvos os datasets convertidos '''
+''' 
+Função que converte os arquivo para um formato simplificado
+Entrada: caminhoEntrada = caminho relativo do diretório dos arquivos que serão convertidos
+         caminhoSaida = caminho relativo do diretório onde serão salvos os arquivos convertidos
+
+Formato do arquivo de entrada:
+  NAME : <nome_do_arquivo>
+  COMMENT : <comentário_sobre_a_instancia>
+  TYPE : <tipo_de_problema>
+  DIMENSION : <N>
+  EDGE_WEIGHT_TYPE : <tipo_dado_da_coordenada> 
+  CAPACITY : <capacidade>
+  NODE_COORD_SECTION 
+  <id_no_1> <coord_x_no_1> <coord_y_no_1>
+  <id_no_2> <coord_x_no_2> <coord_y_no_2>
+  ...
+  <id_no_N> <demanda_no_N> <coord_y_no_N>
+  DEMAND_SECTION 
+  <id_no_1> <demanda_no_1>
+  <id_no_2> <demanda_no_2>
+  ...
+  <id_no_N> <demanda_no_N>
+  DEPOT_SECTION 
+  <id_no_deposito>  
+  -1  
+  EOF 
+
+Formato do arquivo convertido:
+  <N>
+  <capacidade>
+  <id_no_1> <coord_x_no_1> <coord_y_no_1>
+  <id_no_2> <coord_x_no_2> <coord_y_no_2>
+  ...
+  <id_no_N> <coord_x_no_N> <coord_y_no_N>
+  <id_no_1> <demanda_no_1>
+  <id_no_2> <demanda_no_2>
+  ...   
+  <id_no_N> <demanda_no_N> 
+'''
 def converte_dataset(caminhoEntrada, caminhoSaida):
 
   nomes = [nome for nome in os.listdir(caminhoEntrada)]
@@ -33,7 +69,7 @@ def converte_dataset(caminhoEntrada, caminhoSaida):
 
       if depot == 1:
         l = [s for s in line.split() if s.isdigit()]
-        a = l[0]
+        d = l[0]
 
       if line.find('DEPOT_SECTION') != -1:
         demanda = 0
@@ -56,10 +92,10 @@ def converte_dataset(caminhoEntrada, caminhoSaida):
       if line.find('NODE_COORD_SECTION') != -1:
         coord = 1
 
-    if(a == '1'):
-      print('Arquivo' + nome + ' convertido com sucesso')
+    if(d == '1'):
+      print('Arquivo ' + nome + ' convertido com sucesso')
     else:
-      print('[Erro]: Arquivo ' + nome + ' possui um armazem diferente de 1')
+      print('[Erro]: Arquivo ' + nome + ' possui mais de um deposito')
 
     arqEntrada.close()
     arqSaida.close()
@@ -67,39 +103,3 @@ def converte_dataset(caminhoEntrada, caminhoSaida):
 # Chamada da função converte_dataset()
 # Parãmetros: [1]caminhoEntrada, [2]caminhoSaida
 converte_dataset(sys.argv[1], sys.argv[2])
-
-'''
-Formato do dataset de entrada:
-  NAME : <nome_do_arquivo>
-  COMMENT : <comentário_sobre_a_instancia>
-  TYPE : <tipo_de_problema>
-  DIMENSION : <N>
-  EDGE_WEIGHT_TYPE : <tipo_dado_da_coordenada> 
-  CAPACITY : <capacidade>
-  NODE_COORD_SECTION 
-  <id_no_1> <coord_x_no_1> <coord_y_no_1>
-  <id_no_2> <coord_x_no_2> <coord_y_no_2>
-  ...
-  <id_no_N> <demanda_no_N> <coord_y_no_N>
-  DEMAND_SECTION 
-  <id_no_1> <demanda_no_1>
-  <id_no_2> <demanda_no_2>
-  ...
-  <id_no_N> <demanda_no_N>
-  DEPOT_SECTION 
-  <id_no_deposito>  
-  -1  
-  EOF 
-
-Formato do dataset convertido:
-  <N>
-  <capacidade>
-  <id_no_1> <coord_x_no_1> <coord_y_no_1>
-  <id_no_2> <coord_x_no_2> <coord_y_no_2>
-  ...
-  <id_no_N> <coord_x_no_N> <coord_y_no_N>
-  <id_no_1> <demanda_no_1>
-  <id_no_2> <demanda_no_2>
-  ...
-  <id_no_N> <demanda_no_N>
-'''
