42c5bd74cb77b44683dce07f429e698b7ba138d3
EdersonCR <edersonrodrigues10@hotmail.com>
2020-08-29 18:02:57 -0300

    Add geraScriptExecução.py

---
+++ programas_auxiliares/geraScriptExecucao.py
@@ -0,0 +1,49 @@
+import sys
+import os
+import platform
+
+''' Função que gera um arquivo .bat de execução dos datasets
+    Entrada: caminhoDatasets = caminho do diretório dos datasets que serão excutados
+            caminhoMain = caminho do diretório do arquivo main.py
+            conjuntoDataset = letra incial dos arquivos do conjunto dos dataset ('all' para todos conjuntos) '''
+def geraScriptExecucao(caminhoDatasets, caminhoMain, conjuntoDataset):
+
+  if conjuntoDataset == 'all':
+    datasets = [dataset for dataset in os.listdir(caminhoDatasets) if dataset[-4:] == '.vrp']
+  else:
+    datasets = [dataset for dataset in os.listdir(caminhoDatasets) if dataset[0] == conjuntoDataset and dataset[-4:] == '.vrp']
+    conjuntoDataset = 'conj-' + conjuntoDataset
+
+  # Identifica o SO 
+  sisOp = platform.system() 
+
+  if sisOp == 'Linux':
+
+    script = '#!/bin/bash\n'
+    
+    for dataset in datasets:
+      q = [s for s in dataset[:-4].split('k') if s.isdigit()] 
+      comando = 'python3 main.py ' + dataset[:-4] + ' ' + q[0] + '\n'
+      script += '\necho ' + comando + comando
+
+    script += '\necho Todas instâncias foram executadas\n'
+    script += 'read -p "Pressione [Enter] pra fechar...\"'
+
+    extensao = '.sh'
+    nome = 'executa_' + sisOp + '_' + conjuntoDataset + extensao
+
+  arqSaida = open (caminhoMain + '/' + nome, 'w+')
+
+  if arqSaida.write(script):
+    print('Script ' + nome + ' criado com sucesso')
+  else:
+    print('[Erro]: Falha ao escrever no script' + nome)
+
+  arqSaida.close()
+
+  if sisOp == 'Linux':
+    os.system('chmod +x ' + caminhoMain + '/' + nome) # Permissão para o arquivbo ser um executável
+
+''' Chamada da função geraScriptExecucao()
+    Parâmetros: [1]caminhoDatasets, [2]caminhoMain, [3]conjuntoDataset '''
+geraScriptExecucao(sys.argv[1], sys.argv[2], sys.argv[3])
