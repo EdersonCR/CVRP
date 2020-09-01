fd14a6a3f5a9af41894c793d83e5690b86ccbe5d
GahRochaPessoa <gabrielrocha828@gmail.com>
2020-09-01 19:52:58 -0300

    Finalização da plotagem de graficos e criação de matrizes de distancias

--- codigo/main.py
+++ codigo/main.py
@@ -1,8 +1,18 @@
 import numpy as np
 import pandas as pd
 import sys
+import matplotlib.pyplot as plt
 
-caminhoEntrada = '../datatest'
+import Inicializacao as init
+
+caminhoEntrada = '../datasets'
+
+
+def calculaDistancia(pontoAX, pontoAY, pontoBX, pontoBY):
+
+    distancia = ((pontoBX - pontoAX) ** 2 + (pontoBY - pontoAY) ** 2) ** (1/2)
+    return distancia
+
 
 ''' Função principal que executa o método heuristicos de otimização em uma instância de CVRP
     Entrada: dataset: Nome da instância
@@ -14,21 +24,53 @@ def main(dataset):
     print(dataset + '.vrp')
 
     arqEntrada = open(caminhoEntrada + '/' + dataset + '.vrp', 'r')
+    print(arqEntrada)
 
-    qtdeNos = int(arqEntrada.readline(1))
-    capacidade = int(arqEntrada.readline(1))
+    qtdeNos = int(arqEntrada.readline())
+
+    print(qtdeNos)
+
+    capacidade = int(arqEntrada.readline())
     coordenadas = {}
     demandas = []
 
     for i in range(qtdeNos):
-        infoNo = [d for d in arqEntrada.readline(1).split()]
-        coordenadas[int(infoNo[0])] = (int(infoNo[1]), int(infoNo[2]))
+        infoNo = [d for d in arqEntrada.readline().split()]
+        coordenadas[int(infoNo[0])] = [float(infoNo[1]), float(infoNo[2])]
 
     for i in range(qtdeNos):
-        infoNo = [d for d in arqEntrada.readline(1).split()]
+        infoNo = [d for d in arqEntrada.readline().split()]
         demandas.append(int(infoNo[1]))
 
+    matriz = []
+    for i in range(qtdeNos):
+        matriz.append([0.0] * qtdeNos)
+
+    for i in range(qtdeNos):
+        for j in range(qtdeNos):
+            matriz[i][j] = matriz[j][i] = calculaDistancia(
+                coordenadas[i][0], coordenadas[i][1], coordenadas[j][0], coordenadas[j][1])
+
+    x = []
+    y = []
+    for i in range(qtdeNos):
+        x.append(coordenadas[i][0])
+        y.append(coordenadas[i][1])
+
+    titulo = "Instância " + dataset
+
+    plt.title(titulo)
+
+    plt.plot(x, y, color="#000000", linestyle="")
+    plt.scatter(x, y, label="Clientes", color="g",
+                marker=".", s=[no*5 for no in demandas])
+    plt.scatter(x[0], y[0], label="Depósito", color="r", marker=".", s=20)
+    plt.legend()
+    plt.show()
+    plt.savefig("exemplo1.png", dpi=300)
+
     print(coordenadas)
+    print()
     print(demandas)
 
     '''
@@ -63,5 +105,5 @@ def main(dataset):
 
 ''' Chamada da função converte_dataset()
     Parâmetros: [1]dataset, [2]qtdeVeiculos'''
-if __name__ == '__main__':
-    main(sys.argv[1])
+# if __name__ == '__main__':
+main(sys.argv[1])
