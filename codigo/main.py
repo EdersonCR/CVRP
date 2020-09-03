c3bc20a388b3c8b9c670b702f724574ef2ffc460
GahRochaPessoa <gabrielrocha828@gmail.com>
2020-09-03 11:12:24 -0300

    inclusao do print para teste

--- codigo/main.py
+++ codigo/main.py
@@ -5,22 +5,26 @@ from Saida import (salvar as save, visualizar as vis)
 
 ''' Função principal que executa um método heuristico de otimização em uma instância de CVRP
     Entrada: dataset: Nome da instância '''
+
+
 def main(dataset):
+    print('Vai agora')
 
-  # Entrada de dados
-  (qtdeNos, capacVeiculo, coordenadas, demandas) = dt.leitura(dataset)
+    # Entrada de dados
+    (qtdeNos, capacVeiculo, coordenadas, demandas) = dt.leitura(dataset)
 
-  # Preprocessamento
-  distancias = dist.geraMatrizDistancia(coordenadas) 
+    # Preprocessamento
+    distancias = dist.geraMatrizDistancia(coordenadas)
 
-  # Saida de dados
-  save.plotPontos(coordenadas, dataset)
-  #save.plotPontos(coordenadas, dataset, demandas)
-  save.matrizDistancia(distancias, dataset)
-  #vis.printDadosDataset(qtdeNos, capacVeiculo, coordenadas, demandas)
-  #vis.printMatrizDistancia(distancias)
+    # Saida de dados
+    save.plotPontos(coordenadas, dataset)
+    #save.plotPontos(coordenadas, dataset, demandas)
+    save.matrizDistancia(distancias, dataset)
+    #vis.printDadosDataset(qtdeNos, capacVeiculo, coordenadas, demandas)
+    # vis.printMatrizDistancia(distancias)
+
 
 ''' Chamada da função main()
     Parâmetros: [1]nomeDataset'''
 if __name__ == '__main__':
-  main(sys.argv[1])
+    main(sys.argv[1])
