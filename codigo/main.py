60a0bcd1e4c094c9565da26ebff85fb2e33b595a
EdersonCR <edersonrodrigues10@hotmail.com>
2020-09-16 10:42:42 -0300

    Alteração na main.py

--- codigo/main.py
+++ codigo/main.py
@@ -4,23 +4,20 @@ from Preprocessamento import distancia as dist
 from Saida import (salvar as save, visualizar as vis)
 
 ''' Função principal que executa um método heuristico de otimização em uma instância de CVRP
-    Entrada: dataset: Nome da instância '''
+    Entrada: dataset: Nome da instância (sem extensão)'''
 def main(dataset):
 
-    # Entrada de dados
     (qtdeNos, capacVeiculo, coordenadas, demandas) = dt.leitura(dataset)
 
-    # Preprocessamento
     distancias = dist.geraMatrizDistancia(coordenadas)
 
-    # Saida de dados
     save.plotPontos(coordenadas, dataset)
-    # save.plotPontos(coordenadas, dataset, demandas)
+    save.plotPontos(coordenadas, dataset, demandas)
     save.matrizDistancia(distancias, dataset)
-    # vis.printDadosDataset(qtdeNos, capacVeiculo, coordenadas, demandas)
-    # vis.printMatrizDistancia(distancias)
-
-
+    
+	vis.printDadosDataset(qtdeNos, capacVeiculo, coordenadas, demandas)
+    vis.matrizDistancia(distancias)
+    
 ''' Chamada da função main()
     Parâmetros: [1]nomeDataset'''
 if __name__ == '__main__':
