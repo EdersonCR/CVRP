b2d338e87ca689e1438d66c8fa11da5732eecd3c
EdersonCR <edersonrodrigues10@hotmail.com>
2020-09-25 01:32:16 -0300

    Adicionado método que calcula a função objetivo e método calcula o GAP %

--- codigo/Calculo/calculo.py
+++ codigo/Calculo/calculo.py
@@ -1,9 +1,33 @@
 from math import sqrt
 
-''' Função que calcula a distância entre dois pontos
+''' Função que calcula a distância entre dois pontos (arredonda o valor para o inteiro mais próximo)
     Entrada: pontoA = (x, y) coordenasdas x e y do ponto A 
              pontoB = (x, y) coordenasdas x e y do ponto A 
     Saida: distancia = distância entre os pontos A e B'''
 def distanciaPontos(pontoA, pontoB):
-    distancia = sqrt((pontoB[0] - pontoA[0]) ** 2 + (pontoB[1] - pontoA[1]) ** 2)
-    return distancia
+  distancia = int(round(sqrt((pontoB[0] - pontoA[0]) ** 2 + (pontoB[1] - pontoA[1]) ** 2)))
+  return distancia
+
+
+''' Função que calcula o somatório das distâncias de todas as rotas
+    Entrada: rotas = {id_rota: [ nós ]} dicionário com as listas de nós das rotas 
+             distancias = {id: [ distancias ]} dicionario com as listas das distancias entre os pontos 
+    Saida: custo = somatório das distâncias de todas as rotas'''
+def funcaoObjetivo(rotas, distancias):
+
+  custo = 0
+
+  for rota in rotas.values():
+    k = len(rota)
+    for i in range(k-1):
+      custo += distancias[rota[i]][rota[i+1]]
+
+    custo += distancias[0][rota[0]]
+    custo += distancias[rota[-1]][0]
+  
+  return custo
+
+'''
+'''
+def gap(custo, custoMelhor):
+    return (custo - custoMelhor) / custoMelhor * 100
