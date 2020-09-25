1663a6ec57853e02bacb2e9ebd05f189cedc7b97
EdersonCR <edersonrodrigues10@hotmail.com>
2020-09-25 01:33:22 -0300

    Add plotagem da imagem como opicional, tabulação dos resultados e calculo da função objetivo e do GAP

--- codigo/main.py
+++ codigo/main.py
@@ -1,29 +1,43 @@
 import sys
+from time import time, sleep
 from EntradaSaida import (dataset as dt, distancia as dist, solucao as sol)
 from Preprocessamento import preprocessamento as pre
+from Calculo import calculo as calc
 
 ''' Função principal que executa um método heuristico de otimização em uma instância de CVRP
-    Entrada: dataset: Nome da instância (sem extensão)'''
-def main(dataset):
+    Entrada: instancia = Nome da instância (sem extensão)
+             imagem = Indicador se deve ser gerada as imagens da instância (se imagem = 'comImg' gera imagens)
+             rotulo = Indicador se a imagem da instância de ser gerada com rótulo nos nós (se rotulo = 'comRot' gera rotulos) '''
+def main(instancia, imagem, rotulo):
 
-  (qtdeNos, capacVeiculo, coordenadas, demandas) = dt.leituraDataset(dataset)
-  (custoMelhor, solOtimaMelhor, qtdeRotasMelhor, rotasMelhor) = sol.leituraMelhorSolucao(dataset)
+  # Entrada de dados
+  (qtdeNos, capacVeiculo, coordenadas, demandas) = dt.leituraDataset(instancia)
+  (custoMelhor, solOtimaMelhor, qtdeRotasMelhor, rotasMelhor) = sol.leituraMelhorSolucao(instancia)
 
+  # Preprocessamento
   distancias = pre.geraDicionarioDistancia(coordenadas)
 
-  dt.plotDataset(coordenadas, dataset)
-  dt.plotDataset(coordenadas, dataset, demandas)
-  sol.plotSolucao(coordenadas, dataset, rotasMelhor)
-  sol.plotSolucao(coordenadas, dataset, rotasMelhor, demandas)
+  # Calculo funcação objetivo
+  t_inicio = time()
 
-  dist.saveDicionarioDistancia(distancias, dataset)
-  sol.saveSolucao(custoMelhor, solOtimaMelhor, qtdeRotasMelhor, rotasMelhor, dataset)
+  custo = calc.funcaoObjetivo(rotasMelhor, distancias)
+  sleep(qtdeNos/1000)
 
-  # dt.printDataset(qtdeNos, capacVeiculo, coordenadas, demandas)
-  # dist.printDicionarioDistancia(distancias)
-  # sol.printSolucao(custoMelhor, solOtimaMelhor, qtdeRotasMelhor, rotasMelhor)
+  gap = calc.gap(custo, custoMelhor)
+  
+  t_total = time() - t_inicio
+
+  # Salvar dados em arquivo
+  #sol.saveSolucao(custo, t_total, rotasMelhor, instancia)
+  sol.tabulacaoResultado(instancia, custo, t_total, custoMelhor, solOtimaMelhor, gap, rotasMelhor)
+
+  if imagem == 'comImg':
+    dt.plotDataset(coordenadas, instancia, rotulo)
+    dt.plotDataset(coordenadas, instancia, rotulo, demandas)
+    sol.plotSolucao(coordenadas, rotasMelhor, instancia, rotulo)
+    sol.plotSolucao(coordenadas, rotasMelhor, instancia, rotulo, demandas)
     
 ''' Chamada da função main()
-  Parâmetros: [1]nomeDataset'''
+  Parâmetros: [1]nomeInstancia, [2]gerarImagens, [3]gerarRotuloImagens'''
 if __name__ == '__main__':
-  main(sys.argv[1])
+  main(sys.argv[1], sys.argv[2], sys.argv[3])
