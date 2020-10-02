751ca2cceb1dc46cc86b85aa411968d13e98cb7d
EdersonCR <edersonrodrigues10@hotmail.com>
2020-10-02 01:08:02 -0300

    Alterado para gerar scripts de execução com parâmetros personalizados pelo usuario

--- programas_auxiliares/geraScriptExecucao.py
+++ programas_auxiliares/geraScriptExecucao.py
@@ -2,16 +2,22 @@ import sys
 import os
 import platform
 
+CAMINHO_MAIN = '../codigo/'
+CAMINHO_DATASETS = '../dados/datasets'
+
 ''' Função que gera um arquivo .bat de execução dos datasets
-    Entrada: caminhoDatasets = caminho do diretório dos datasets que serão excutados
-             caminhoMain = caminho do diretório do arquivo main.py
-             conjuntoDataset = letra incial dos arquivos do conjunto dos dataset ('all' para todos conjuntos) '''
-def geraScriptExecucao(caminhoDatasets, caminhoMain, conjuntoDataset):
+    Entrada: conjuntoDataset = letra incial dos arquivos do conjunto dos dataset ('all' para todos conjuntos)
+             paramentros = string com os parametros que serão passados para a função main() para execução '''
+def geraScriptExecucao(conjuntoDataset, parametros):
+
+  extensao = ''
+  script = ''
+  parametros = parametros.replace('\"', '')
 
   if conjuntoDataset == 'all':
-    datasets = [dataset for dataset in os.listdir(caminhoDatasets) if dataset[-4:] == '.vrp']
+    datasets = [dataset for dataset in os.listdir(CAMINHO_DATASETS) if dataset[-4:] == '.vrp']
   else:
-    datasets = [dataset for dataset in os.listdir(caminhoDatasets) if dataset[0] == conjuntoDataset and dataset[-4:] == '.vrp']
+    datasets = [dataset for dataset in os.listdir(CAMINHO_DATASETS) if dataset[0] == conjuntoDataset and dataset[-4:] == '.vrp']
     conjuntoDataset = 'conj-' + conjuntoDataset
 
   datasets.sort()
@@ -24,7 +30,7 @@ def geraScriptExecucao(caminhoDatasets, caminhoMain, conjuntoDataset):
     script = '#!/bin/bash\n'
     
     for dataset in datasets:
-      comando = 'python3 main.py ' + dataset[:-4] + ' semImg semRot'
+      comando = 'python3 main.py ' + dataset[:-4] + ' ' + parametros
       script += '\necho -e "\\n' + comando + '"\n' + comando + '\n'
 
     script += '\necho -e "\\nTodas instâncias foram executadas"\n'
@@ -37,16 +43,16 @@ def geraScriptExecucao(caminhoDatasets, caminhoMain, conjuntoDataset):
     script = 'echo off\n'
     
     for dataset in datasets:
-      comando = 'python main.py ' + dataset[:-4] + ' semImg semRot'
-      script += '\necho.\necho "' + comando + '"\n' + comando + '\n'
+      comando = 'python main.py ' + dataset[:-4] + ' ' + parametros
+      script += '\necho.\necho ' + comando + '\n' + comando + '\n'
 
     script += '\necho.\necho "Todas instâncias foram executadas"\n'
     script += 'pause'
 
     extensao = '.bat'
   
-  nome = 'executa_' + sisOp + '_' + conjuntoDataset + extensao
-  arqSaida = open (caminhoMain + '/' + nome, 'w+')
+  nome = 'executa_' + sisOp + '_' + conjuntoDataset + '_' + parametros.replace(' ', '_') + extensao
+  arqSaida = open (CAMINHO_MAIN + '/' + nome, 'w+')
 
   if arqSaida.write(script):
     print('Script ' + nome + ' criado com sucesso')
@@ -56,8 +62,8 @@ def geraScriptExecucao(caminhoDatasets, caminhoMain, conjuntoDataset):
   arqSaida.close()
 
   if sisOp == 'Linux':
-    os.system('chmod +x ' + caminhoMain + '/' + nome) # Permissão para o arquivo ser um executável
+    os.system('chmod +x ' + CAMINHO_MAIN + '/' + nome) # Permissão para o arquivo ser um executável
 
 ''' Chamada da função geraScriptExecucao()
-    Parâmetros: [1]caminhoDatasets, [2]caminhoMain, [3]conjuntoDataset '''
-geraScriptExecucao(sys.argv[1], sys.argv[2], sys.argv[3])
+    Parâmetros: [1]conjuntoDataset, [2]parametros '''
+geraScriptExecucao(sys.argv[1], sys.argv[2])
