7c37f324cec3e4f73035ae47841a6f3101e4bb6e
EdersonCR <edersonrodrigues10@hotmail.com>
2020-10-16 21:28:11 -0300

    Alterado para receber a semente como parâmetro

--- codigo/Construcao/aleatorio.py
+++ codigo/Construcao/aleatorio.py
@@ -1,14 +1,14 @@
 from random import *
-from Construcao import SEMENTE
 
 ''' Função que executa uma heuristica aleatoria para definir uma solução para CVRP
     Entrada: qtdeNos = quantidade de nós (clientes + depósito)
              capacVeiculo = capacidade de carga do veículo
              demandas = lista de demandas dos nós, id do nó = índice da lista
+             semente = valor incial passado para gerador de numeros pseudoaletório
     Saida: rota = [ nós ] listas com a ordem de visitação de nós nas rotas '''
-def heuristicaAleatoria(qtdeNos, capacVeiculo, demandas):
+def heuristicaAleatoria(qtdeNos, capacVeiculo, demandas, semente):
 
-  seed(SEMENTE)
+  seed(semente)
 
   clientes = [c for c in range(1, qtdeNos)]
   clientes = sample(clientes, len(clientes))
